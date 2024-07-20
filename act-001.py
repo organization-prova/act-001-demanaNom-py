da060ee72cb228dfe8d3275b423ea3e49b514883
joanpardogine <63462877+joanpardogine@users.noreply.github.com>
2024-07-20 13:05:54 +0200

    Update act-001.py

--- act-001.py
+++ act-001.py
@@ -1,5 +1,6 @@
 def demanaNom()
-  print("Encara no està feta l'activitat!")
+  // Desenvolupa aquí el teu codi
+  print("Encara no està feta l'activitat!") // Cal eliminar aquesta línia
 
 # Clàusula per executar l'aplicació
 if __name__ == '__main__':
